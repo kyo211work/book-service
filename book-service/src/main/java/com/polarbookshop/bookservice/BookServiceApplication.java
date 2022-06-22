d6253e5f1dad58d04abea9d6b5dc361ea62b31c0
Kiwaun Wallace <KiwaunW@quinnox.com>
2022-06-22 09:15:48 -0500

    first commit

---
+++ book-service/src/main/java/com/polarbookshop/bookservice/BookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.bookservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookServiceApplication {
+
+	public static void main(String[] arg
+		SpringApplication.run(BookServiceApplication.class, args);
+	}
+
+}
---
+++ book-service/src/main/java/com/polarbookshop/bookservice/SpringBootApplication.java
@@ -0,0 +1,5 @@
+package com.polarbookshop.bookservice;
+
+public @interface SpringBootApplication {
+
+}
